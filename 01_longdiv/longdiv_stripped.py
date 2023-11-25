e5b346dae9170d57184d4880449a84f1241621cf
Naberno <burusov.vl@gmail.com>
2023-11-25 17:34:27 +0500

    Готовое

--- 01_longdiv/longdiv_stripped.py
+++ 01_longdiv/longdiv_stripped.py
@@ -10,11 +10,11 @@ def long_division(dividend, divider):
     >>> 12345÷25
     12345|25
     100  |493
-    234
-    225
-    95
-    75
-    20
+     234
+     225
+      95
+      75
+      20
 
     >>> 1234÷1423
     1234|1423
@@ -31,35 +31,24 @@ def long_division(dividend, divider):
          1
     '''
     #INSERT CODE HERE
-def long_division(dividend, divider):
-    quotient = dividend // divider
-    remainder = dividend % divider
-    probel = " " * (len(str(dividend)))
+    number = list(map(int, str(dividend)))
+    result = f" _{dividend}│{divider}\n"
+    lenofdig = " " * len(number)
+    answ = dividend // divider
+    result += f"{lenofdig}  │{answ}\r"
 
-    if divider == 0:
-        result = "Делить на 0 нельзя"
-    elif dividend == 0:
-        result = f"{dividend}|{divider}\n |0"
-    elif divider > dividend:
-        result = f" {dividend}|{divider}\n‾{dividend}|0\n{probel}0"
-    elif dividend == divider:
-        result = f" {dividend}|{divider}\n‾{dividend}|1\n{probel}0"
-    else:
-        number = (divider * int(str(quotient)[0]))
-        probel2 = " " * (len(str(dividend)) - len(str(number)))
-        result = f" {dividend}|{divider}\n‾{number}{probel2}|{quotient}\n"#{probel}{remainder}\n
-        for i in range(0,len(str(dividend))-1):
-            digit = int(str(quotient)[i]) if i < len(str(quotient)) else 0
-            number = (divider * digit) if i < len(str(quotient)) else (remainder * 10)
-            if i == 0:
-                number_2 = ((int(str(dividend)[i:len(str(number))+i])-number) * 10 + int(str(dividend)[len(str(number))-1:len(str(number))]))
-            else:
-                number_2 = (number_2 - number) * 10 + int(str(dividend)[len(str(number))-1:len(str(number))])
-            # print("1:",number_2, "- number= ",number)
-            # print("@2",number_2)
-            # print(i)
-            result += f"{probel2} {number_2}\n{probel2}‾{number}\n"
-        return result
+    reminder = probel = 0
+    while number:
+        reminder = reminder * 10 + number.pop(0)
+        if reminder >= divider:
+            if probel:
+                result += f"{'':<{probel}} _{reminder}\n"
+            l = len(str(reminder))
+            integer, reminder = divmod(reminder, divider)
+            result += f"{'':<{probel}}  {integer * divider:>{l}}\n"
+            probel += l - len(str(reminder)) + (0 if reminder else 1)
+    
+    result += f"{'':<{len(str(dividend))-len(str(reminder))}}  {reminder}"
     return result
 
 def main():
@@ -88,9 +77,6 @@ def main():
     print(long_division(123456789, 531))
     print()
     print(long_division(425934261694251, 12345678))
-    input("Press any key to exit...")
-
 
 if __name__ == '__main__':
     main()
-    
